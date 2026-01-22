285c38c9d4860f4e494c51647f8d853979ca7d30
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2026-01-22 01:08:33 +0100

    Add downloading script

---
+++ download_headers.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+BASE_URL=https://docs.oasis-open.org/pkcs11/pkcs11-spec/v3.1/os/include/pkcs11-v3.1/
+
+wget -P include ${BASE_URL}pkcs11.h
+wget -P include ${BASE_URL}pkcs11f.h
+wget -P include ${BASE_URL}pkcs11t.h
