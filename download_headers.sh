df3208f97683f1fad05c1b384e7d1dac5c0eb98f
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2026-01-23 03:58:11 +0100

    Add base URL for version 3.0

--- download_headers.sh
+++ download_headers.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#BASE_URL=https://docs.oasis-open.org/pkcs11/pkcs11-base/v3.0/os/include/pkcs11-v3.0/
 BASE_URL=https://docs.oasis-open.org/pkcs11/pkcs11-spec/v3.1/os/include/pkcs11-v3.1/
 
 wget -P include ${BASE_URL}pkcs11.h
