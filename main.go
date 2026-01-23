1dd8cd0637d4dfb0208156cf21c4cd84a7d5b1aa
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2026-01-23 14:42:12 +0100

    Add function to function list

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ package main
 import "C"
 import "fmt"
 
-var functionList = C.CK_FUNCTION_LIST{}
+var functionList = C.CK_FUNCTION_LIST{
+	C_Initialize: *(*C.CK_C_Initialize)(C.C_Initialize),
+}
 
 func main() {}
 
